1f9f01691b0b93277c24beb49265a525784071b5
emmy70 <emmanuelusman701@gmail.com>
2022-01-22 04:47:43 +0100

    while && for loop test completed

--- Demo/js/main.js
+++ Demo/js/main.js
@@ -1,60 +1,56 @@
-var x = 1;
-var y = 2;
+// var outList = 0;
 
-console.log("2" == y && x === 1);
+// while (outList < 4) {
+//   console.log("hello")
+//   outList = outList +1
+// }
 
-console.log(x >= 0 || y === "2");
 
-console.log(!(x !== 1) && y === 1 + 1);
+///////////////////////////////////////
+//////////   PROBLEM 1 ///////////////
+///////////////////////////////////////
 
-console.log(y !== "2" && x < 10);
+// Use a For Loop to print (console.log()) out the word "hello" 5
 
-console.log(y !== x || y == "2" || x === 3);
+//
 
-// Write a While Loop that prints out
-// only the even numbers from 1 to 10.
+// Do this with a While Loop and a For Loop
 
-// my Answer
+// While Loop
 
-var a = 0;
+// var outList = 0;
 
-while (a < 11) {
-  console.log("here is our even number ======> " + a);
+// while (outList < 5) {
+//   console.log("hello");
+//   outList = outList + 1;
+// }
 
-  a = a + 2;
+// For Loop
+// var sayHello = 5;
+
+for (var y = 0; y <= 4; y++) {
+  console.log("hello");
 }
 
-// instructor's solution
 
-var num = 1;
+///////////////////////////////////////
+//////////   PROBLEM 1 ///////////////
+///////////////////////////////////////
 
-while (num < 11) {
-  console.log(num);
-  num = num + 1;
+// Use Loops to console.log() (print out) all the odd numbers from 1 25
+// Do this using two methods, a while Loop and a for Loop
+
+// Method One
+// While Loop
+
+var countOdd = 1;
+
+while (countOdd < 26) {
+  console.log(countOdd);
+  countOdd = countOdd + 2;
 }
 
-// solutin 2
-
-var getEven = 1;
-
-while (getEven < 11) {
-  if (getEven % 2 === 0) {
-    console.log(getEven);
-  }
-  getEven = getEven + 1;
+for (var oddNum = 1; oddNum < 26; oddNum++){
+  console.log(oddNum);
+  ++oddNum
 }
-
-var num1 = 0;
-
-while (num1 < 6) {
-  console.log("hello word");
-  num = num + 1;
-}
-
-var cooutNum = 0;
-
-while (cooutNum < 11) {
-  console.log("Yay counted even number ===> " + cooutNum);
-  cooutNum = cooutNum + 2;
-}
- 
